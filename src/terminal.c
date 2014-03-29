687a3512ee94c5e6cd400cb5c0cc3b23c529ff6c
marteld <marteld@c9f86b0d-2f9a-42de-980a-b0dfa8591743>
2014-03-29 00:27:29 +0000

    first implementation of terminal
    git-svn-id: https://svnedu.esiee.fr/svn/PR3602_111_marteld@12 c9f86b0d-2f9a-42de-980a-b0dfa8591743

--- src/terminal.c
+++ src/terminal.c
@@ -7,11 +7,11 @@ int main(int argc, char* argv[]){
 	if(argc == 3){
 		int writeFD = atoi(argv[1]);
 		int readFD = atoi(argv[2]);
-		char* firstLine = litLigne(readFD);
-		printf("%s\n",firstLine);
-		free(firstLine);
+		dup2(readFD, 0);
+		dup2(writeFD, 1);
 		close(writeFD);
-		close(readFD);				
+		close(readFD);
+/*		...	*/
 	}else{
 		printf("usage:\n%s <writing file descriptor> "
 				 "<reading file descriptor>\n",argv[0]);
