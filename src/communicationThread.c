d1ee1b555bc7028981b070ef6b73bcee5fd0a154
marteld <marteld@c9f86b0d-2f9a-42de-980a-b0dfa8591743>
2014-03-29 00:27:38 +0000

    fix communication fonction
    git-svn-id: https://svnedu.esiee.fr/svn/PR3602_111_marteld@18 c9f86b0d-2f9a-42de-980a-b0dfa8591743

--- src/communicationThread.c
+++ src/communicationThread.c
@@ -1,14 +1,16 @@
 #include "communicationThread.h"
+#include <unistd.h>
+#include <stdlib.h>
 
-pthread_t* createThread(void* (*start_routine)(void*), int *parentPipe[2]){
-	pthread_t *threadId;
-	int *childPipe[2];
+pthread_t createThread(void* (*start_routine)(void*), int parentPipe[2]){
+	pthread_t threadId;
+	int *childPipe = malloc(2 * sizeof (int));
+	pipe(parentPipe);
 	pipe(childPipe);
-	
-	int writePipe = *parentPipe[WRITE];
-	*parentPipe[WRITE] = *childPipe[WRITE];
-	*childPipe[WRITE] = fd;
+	int writePipe = parentPipe[WRITE];
+	parentPipe[WRITE] = childPipe[WRITE];
+	childPipe[WRITE] = writePipe;
 		
-	pthread_create(threadId, start_routine,childPipe);
+	pthread_create(&threadId, NULL, start_routine, (void*)childPipe);
 	return threadId;
 }
