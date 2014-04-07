cad35b49066dc79eaeafc1b318e64e7f105e84e2
marteld <marteld@c9f86b0d-2f9a-42de-980a-b0dfa8591743>
2014-04-07 13:33:49 +0000

    used getopt to parse argv
    git-svn-id: https://svnedu.esiee.fr/svn/PR3602_111_marteld@27 c9f86b0d-2f9a-42de-980a-b0dfa8591743

--- src/autorisation.c
+++ src/autorisation.c
@@ -2,19 +2,53 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include "../libCarteBancaire/lectureEcriture.h"
+#include <getopt.h>
+
+struct option longopts[] = {
+	{"input",	required_argument, 0, 'i'},
+	{"output",	required_argument, 0, 'o'},
+	{"bank",		required_argument, 0, 'b'}
+};
+
+void printHelp(const char* programName);
 
 int main(int argc, char* argv[]){
-	if(argc == 3){
-		int writeFD = atoi(argv[1]);
-		int readFD = atoi(argv[2]);
+	if(argc == 7){
+		opterr = 0;
+		int indexptr;
+		int opt;
+		int writeFD, readFD;
+		while((opt = getopt_long(argc, argv, "i:o:b:",longopts, &indexptr)) != -1){
+			printf("option: %c - %s\n",opt,optarg);
+			switch(opt){
+				case 'i':
+					break;
+				case 'o':
+					writeFD = atoi(optarg);
+					break;
+				case 'b':
+					readFD = atoi(optarg);
+					break;
+				default:
+					printHelp(argv[0]);
+					break;
+			}
+		}
 		dup2(readFD, 0);
 		dup2(writeFD, 1);
 		close(writeFD);
 		close(readFD);
 /*		...	*/
 	}else{
-		printf("usage:\n%s <writing file descriptor> "
-				 "<reading file descriptor>\n",argv[0]);
+		printHelp(argv[0]);
 	}
- return argc != 3;
+ return argc != 7;
+}
+
+void printHelp(const char* programName){
+	printf(	"Usage : %s [OPTION]...\n"
+				"  -i,--input\t file descriptor to read into (mandatory)\n"
+				"  -o,--output\t file descriptor to write into (mandatory)\n"
+				"  -b,--bank\t bank id (mandatory)\n"
+			,programName);
 }
