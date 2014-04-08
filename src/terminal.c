d4bb1c4a998d7bfa9e5dff899beec618c3192b49
marteld <marteld@c9f86b0d-2f9a-42de-980a-b0dfa8591743>
2014-04-08 08:48:27 +0000

    minimal terminal sending static message
    git-svn-id: https://svnedu.esiee.fr/svn/PR3602_111_marteld@33 c9f86b0d-2f9a-42de-980a-b0dfa8591743

--- src/terminal.c
+++ src/terminal.c
@@ -1,20 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <getopt.h>
 #include "../libCarteBancaire/lectureEcriture.h"
+#include "../libCarteBancaire/message.h"
+
+struct option longopts[] = {
+	{"input",	required_argument, 0, 'i'},
+	{"output",	required_argument, 0, 'o'}
+};
+
+void printHelp(const char* programName);
 
 int main(int argc, char* argv[]){
-	if(argc == 3){
-		int writeFD = atoi(argv[1]);
-		int readFD = atoi(argv[2]);
-		dup2(readFD, 0);
-		dup2(writeFD, 1);
-		close(writeFD);
-		close(readFD);
-/*		...	*/
+	if(argc == 5){
+		opterr = 0;
+		int indexptr;
+		int opt;
+		int readFD, writeFD;
+		while((opt = getopt_long(argc, argv, "i:o:",longopts, &indexptr)) != -1){
+			switch(opt){
+				case 'i':
+					readFD = atoi(optarg);
+					break;
+				case 'o':
+					writeFD = atoi(optarg);
+					break;
+				default:
+					printHelp(argv[0]);
+					break;
+			}
+		}
+/*		char* cardNumber = malloc(16);*/
+/*		char* messageType = malloc(7);*/
+/*		char* value = malloc(100);*/
+/*		char* string;*/
+/*		char end = 0;*/
+/*		while(!end){*/
+/*			cardNumber[0] = '\0';*/
+/*			messageType[0] = '\0';*/
+/*			value[0] = '\0';*/
+/*			string = litLigne(readFD);*/
+/*			if(decoupe(string,cardNumber,messageType,value) == 0){*/
+/*				perror("message in wrong format");*/
+/*				end = 1;*/
+/*			}*/
+/*			if(strcmp(bankId,cardNumber) == 0){*/
+/*				strcpy(ack,"ACK\n");*/
+/*			}else{*/
+/*				strcpy(ack,"NACK\n");*/
+/*			}*/
+			char* msg = message("0234567890123456","Réponse","9");
+			ecritLigne(writeFD,msg);
+			free(msg);
+			msg = litLigne(readFD);
+			printf("%s",msg);
+			free(msg);
+/*		}*/
+/*		free(string);*/
+/*		free(cardNumber);*/
+/*		free(messageType);*/
+/*		free(value);*/
 	}else{
-		printf("usage:\n%s <writing file descriptor> "
-				 "<reading file descriptor>\n",argv[0]);
+		printHelp(argv[0]);
 	}
- return argc != 3;
+ return 0;
 }
+
+void printHelp(const char* programName){
+	fprintf(	stderr,
+				"Usage : %s [OPTION]...\n"
+				"  -i,--input\t file descriptor to read into (mandatory)\n"
+				"  -o,--output\t file descriptor to write into (mandatory)\n",
+				programName
+	);
+}
+
