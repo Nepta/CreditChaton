1d2d1e31fafd76d97c7ee380a5dd877531791bc6
marteld <marteld@c9f86b0d-2f9a-42de-980a-b0dfa8591743>
2014-04-11 04:07:27 +0000

    directory service loading test
    git-svn-id: https://svnedu.esiee.fr/svn/PR3602_111_marteld@55 c9f86b0d-2f9a-42de-980a-b0dfa8591743

--- src/annuaire.c
+++ src/annuaire.c
@@ -1,9 +1,10 @@
+#include <stdlib.h>
 #include "annuaire.h"
 
 int exist(DirectoryService *data, char id[16]){
 	int isIdExisting = 0;
 	for(int i=0; i<data->size; i++){
-		if(strncmp(data->base[i],id,16){
+		if(strncmp(data->base[i],id,16)){
 			isIdExisting = 1;
 			break;
 		}
@@ -11,19 +12,20 @@ int exist(DirectoryService *data, char id[16]){
  return isIdExisting;
 }
 
-DirectoryService* load(int fs){
+DirectoryService* load(int fd){
 	int registeredUser = 10;
 	DirectoryService* data = malloc(sizeof (DirectoryService) + registeredUser * 16);
-	strncpy(data->base[0],"0000000000000000");
-	strncpy(data->base[1],"0000000000000001");
-	strncpy(data->base[2],"0000000000000002");
-	strncpy(data->base[3],"0000000000000003");
-	strncpy(data->base[4],"0000000000000004");
-	strncpy(data->base[5],"0000000000000005");
-	strncpy(data->base[6],"0000000000000006");
-	strncpy(data->base[7],"0000000000000007");
-	strncpy(data->base[8],"0000000000000008");
-	strncpy(data->base[9],"0000000000000009");
+	data->size = registeredUser;
+	strncpy(data->base[0],"0000000000000000",16);
+	strncpy(data->base[1],"0000000000000001",16);
+	strncpy(data->base[2],"0000000000000002",16);
+	strncpy(data->base[3],"0000000000000003",16);
+	strncpy(data->base[4],"0000000000000004",16);
+	strncpy(data->base[5],"0000000000000005",16);
+	strncpy(data->base[6],"0000000000000006",16);
+	strncpy(data->base[7],"0000000000000007",16);
+	strncpy(data->base[8],"0000000000000008",16);
+	strncpy(data->base[9],"0000000000000009",16);
  return data;
 }
 
--- src/annuaire.h
+++ src/annuaire.h
@@ -1,13 +1,15 @@
 #ifndef __ANNUAIRE_H__
 #define __ANNUAIRE_H__
 
+#include <string.h>
+
 typedef struct{
 	int size;
-	char* base[];
+	char base[][16];
 }DirectoryService;
 
 int exist(DirectoryService *data, char id[16]);
-DirectoryService* load(int fs);
+DirectoryService* load(int fd);
 
 #endif /* __ANNUAIRE_H__ */
 
